b2b947a0b4d0888df231307e46463068a6765daa
Henjie Agadan <70084057+hagdn@users.noreply.github.com>
2022-08-04 20:38:48 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//                         By Henjie Agadan
+// Using single external interrupt pin. Baremetal programming for ATMega328p.
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
